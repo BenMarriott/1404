2f8fdfd78519df13dbb5c2c50198da6e247f58ba
benmarriott <benjamin.marriott@my.jcu.edu.au>
2018-03-24 09:16:06 +1000

    Add completed practical exercises

--- prac_04/accountant_annie.py
+++ prac_04/accountant_annie.py
@@ -1,24 +1,28 @@
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+
 def main():
+    """Display income report for incomes over a given number of months."""
     incomes = []
+    monthly_income = int(input("Enter number of incomes to report: "))
 
-    income_months = int(input("Enter number of incomes to report: "))
-
-    # while amount_incomes >= 0:
-    for i in range(income_months):
-        income = int(input("Enter number of income for month {}: ".format(i + 1)))
+    for i in range(monthly_income):
+        income = float(input("Enter number of income for month {}: ".format(i + 1)))
         incomes.append(income)
 
     print("\n Income Report\n" + ("-" * 20))
 
     def print_report():
-        global income
         total = 0
-        for element in incomes:
-            income = incomes[income_months - 1]
+        for month in range(1, monthly_income + 1):
+            income = incomes[month - 1]
             total += income
-            print(" Month {} - Income: $ {} Total: $ {}".format(income_months, element, total))
-
-
+            print(" Month {:2} - Income: $ {:<10.2f} Total: $ {:10.2f}".format(monthly_income, month, total))
+            # TODO does not print correct month.
     print_report()
+
 
 main()
